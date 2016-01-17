88858f19d81d0ee12b5aea183ea92580f82c0a95
Quirin Hirmer <quirin.hirmer@tum.de>
2016-01-17 05:06:25 +0100

    RandomWalk state prototype

---
+++ src/driver/src/StateRandomWalk.cpp
@@ -0,0 +1,10 @@
+#include "StateRandomWalk.h"
+
+StateRandomWalk::StateRandomWalk()
+  : State(StateID::RANDOM_WALK)
+{
+}
+
+void StateRandomWalk::tick()
+{
+}
---
+++ src/driver/src/StateRandomWalk.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "State.h"
+
+class StateRandomWalk : public State
+{
+public:
+  StateRandomWalk();
+
+  virtual void tick();
+
+private:
+
+};
