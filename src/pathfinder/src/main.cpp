a08e7b5d3237daba1fab5e6b7ed04a8e2606629c
Stefan Urban <stefan.urban@tum.de>
2016-01-19 16:46:21 +0100

    added SLAM algorithm and move_base for path planning

--- src/pathfinder/src/main.cpp
+++ src/pathfinder/src/main.cpp
@@ -1,18 +1,23 @@
 #include <ros/ros.h>
-#include <geometry_msgs/PointStamped.h>
+#include <geometry_msgs/Pose.h>
 #include <aruco_msgs/Marker.h>
 #include <aruco_msgs/MarkerArray.h>
 #include <map>
 #include <vector>
+#include <nav_msgs/GetPlan.h>
+#include <nav_msgs/Odometry.h>
 
 #include <pathfinder/Path.h>
-#include <obstacle_detection/Obstacle.h>
-#include <obstacle_detection/ObstacleArray.h>
-
-#include "Pathfinder.h"
 
 
-std::vector<geometry_msgs::PointStamped> marker_positions(8);
+geometry_msgs::Pose current_pose;
+
+void poseCallback(const nav_msgs::Odometry::ConstPtr& odom)
+{
+  current_pose = odom->pose.pose;
+}
+
+std::vector<geometry_msgs::PoseStamped> marker_poses(8);
 
 void markersCallback(const aruco_msgs::MarkerArrayConstPtr& marker_array)
 {
@@ -20,12 +25,12 @@ void markersCallback(const aruco_msgs::MarkerArrayConstPtr& marker_array)
 
   for (aruco_msgs::Marker &marker : markers)
   {
-    geometry_msgs::PointStamped point;
+    geometry_msgs::PoseStamped pose;
 
-    point.header.stamp = ros::Time::now();
-    point.point = marker.pose.pose.position;
+    pose.header.stamp = ros::Time::now();
+    pose.pose = marker.pose.pose;
 
-    marker_positions[marker.id] = point;
+    marker_poses[marker.id] = pose;
   }
 }
 
@@ -39,23 +44,39 @@ int main(int argc, char** argv)
   ros::start();
   ROS_INFO_STREAM("Startup!");
 
+  // Subscribe to current pose
+  ros::Subscriber pose_subscriber = node.subscribe("/odom", 1, poseCallback);
+
   // Subcribe to AruCo markers
   ros::Subscriber markers_sub = node.subscribe("/aruco_marker_publisher/markers", 1, &markersCallback);
 
   // Publisher for available paths
   ros::Publisher path_pub = node.advertise<pathfinder::Path>("/paths", 10);
 
+  // Service for path planning
+  std::string service_name = "/move_base/make_plan";
+
+  while (!ros::service::waitForService(service_name, ros::Duration(5.0)))
+  {
+    ROS_INFO_STREAM("Waiting for service " << service_name << " to become available.");
+  }
+
+  ros::ServiceClient planner_client = node.serviceClient<nav_msgs::GetPlan>(service_name);
+
+  if (!planner_client)
+  {
+    ROS_FATAL("Planner not available!");
+  }
+
+
   // Constantly determine paths to all known AruCo codes
   ros::Rate loop_rate(1.0);
-
-
-  Pathfinder pathfinder;
 
   while (ros::ok())
   {
     unsigned int counter = 0;
 
-    for (auto destination = marker_positions.begin(); destination != marker_positions.end() ; ++destination, ++counter)
+    for (auto destination = marker_poses.begin(); destination != marker_poses.end() ; ++destination, ++counter)
     {
       // Check if AruCo code is even available
       if (destination->header.stamp.sec == 0)
@@ -66,10 +87,10 @@ int main(int argc, char** argv)
       // Check if information is good or outdated
       ros::Duration age = ros::Time::now() - destination->header.stamp;
 
-      if (age.sec > 4)
+      if (age.sec > 10)
       {
         // Delete outdated information
-        marker_positions.erase(marker_positions.begin() + counter);
+        marker_poses.erase(marker_poses.begin() + counter);
 
         continue;
       }
@@ -77,6 +98,29 @@ int main(int argc, char** argv)
       // Calculate waypoints on path
       pathfinder::Path path;
 
+      // Request plan from move base
+      if (!planner_client)
+      {
+        ROS_FATAL("Planner service failed");
+        return -1;
+      }
+
+      nav_msgs::GetPlan request;
+
+      // Current position is start point
+      request.request.start.header.frame_id = "map";
+      request.request.start.pose = current_pose;
+
+      request.request.goal.header.frame_id = "map";
+      request.request.goal.pose = destination->pose;
+
+      request.request.tolerance = 0;
+
+      // Call service
+      planner_client.call(request);
+
+      // Publish
+      path.path = request.response.plan;
       path_pub.publish(path);
     }
 
