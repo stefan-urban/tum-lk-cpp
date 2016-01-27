2b3e2bb0b443ef2e334829f43076443d6dcc30ec
Stefan Urban <stefan.urban@tum.de>
2016-01-27 22:27:40 +0100

    assume the position of the markers does not change, so save it and broadcast its tf periodically

--- src/aruco_tf_broadcaster/src/main.cpp
+++ src/aruco_tf_broadcaster/src/main.cpp
@@ -5,18 +5,50 @@
 #include <aruco_msgs/MarkerArray.h>
 #include <aruco_ros/aruco_ros_utils.h>
 #include <string>
+#include <map>
+#include <vector>
+
+
+std::map<unsigned int, geometry_msgs::PoseStamped> markers_;
 
 void markersCallback(const aruco_msgs::MarkerArrayConstPtr& marker_array)
 {
-  static tf::TransformBroadcaster br;
   std::vector<aruco_msgs::Marker> markers = marker_array->markers;
 
   for (const aruco_msgs::Marker &marker : markers)
   {
-    tf::Pose tf_pose;
-    tf::poseMsgToTF(marker.pose.pose, tf_pose);
+    // Define stamped pose
+    geometry_msgs::PoseStamped pose;
+    pose.pose = marker.pose.pose;
 
-    br.sendTransform(tf::StampedTransform(tf_pose, ros::Time::now(), "markers_link", "marker_" + std::to_string(marker.id)));
+    // @todo: there has to be a function that does this in one line!
+
+    // Find if already in storage
+    std::map<unsigned int, geometry_msgs::PoseStamped>::iterator it = markers_.find(marker.id);
+
+    if( it != markers_.end() )
+    {
+      // Found, replace
+      markers_[marker.id] = pose;
+    }
+    else
+    {
+      // Not found, insert
+      markers_.insert(std::pair<unsigned int, geometry_msgs::PoseStamped>(marker.id, pose));
+    }
+  }
+}
+
+void broadcastTf(const ros::TimerEvent&)
+{
+  static tf::TransformBroadcaster br;
+
+  for (const auto &marker : markers_)
+  {
+    tf::Pose tf_pose;
+    tf::poseMsgToTF(marker.second.pose, tf_pose);
+
+    br.sendTransform(tf::StampedTransform(tf_pose, ros::Time::now(), "markers_link", "marker_" + std::to_string(marker.first)));
   }
 }
 
@@ -28,10 +60,12 @@ int main(int argc, char** argv)
 
   // Start ROS
   ros::start();
-  ROS_INFO_STREAM("Startup!");
 
   // Subscribe all aruco markers
   ros::Subscriber sub = node.subscribe("/aruco_marker/markers", 10, &markersCallback);
+
+  // Send transformations every 100 ms
+  ros::Timer timer = node.createTimer(ros::Duration(0.1), &broadcastTf, false);
 
   // Loop
   ros::spin();
