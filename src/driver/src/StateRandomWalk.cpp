f532428fd11c1aa33bce8530d90550d4290581b9
Quirin Hirmer <quirin.hirmer@tum.de>
2016-02-09 01:28:26 +0100

    Random walk

--- src/driver/src/StateRandomWalk.cpp
+++ src/driver/src/StateRandomWalk.cpp
@@ -1,10 +1,59 @@
 #include "StateRandomWalk.h"
+#include <math.h>
 
-StateRandomWalk::StateRandomWalk()
-  : State(StateID::RANDOM_WALK)
+StateRandomWalk::StateRandomWalk(std::shared_ptr<TurtleBot> turtleBot)
+  : State(StateID::RANDOM_WALK, turtleBot)
 {
+  getTurtleBot()->getNode()->subscribe("scan", 1, &StateRandomWalk::scanCallback, this);
 }
 
 void StateRandomWalk::tick()
 {
+  if(doMove)
+  {
+    getTurtleBot()->move(MOVEMENT_SPEED, 0);
+  } else {
+    getTurtleBot()->move(0, TURN_SPEED * angleFactor);
+  }
+}
+
+void StateRandomWalk::scanCallback(const	sensor_msgs::LaserScan::ConstPtr& scan)
+{
+  // Find the range minimum
+  int min_idx = std::abs(std::ceil(MIN_SCAN_ANGLE_RAD / scan->angle_increment));
+  int max_idx = std::abs(std::floor(MAX_SCAN_ANGLE_RAD / scan->angle_increment));
+
+  int scan_count = std::abs(2*M_PI / scan->angle_increment);
+
+  float closest_range_right = scan->ranges[min_idx];
+  float closest_range_left = scan->ranges[min_idx];
+
+  // scan the right side
+  for (int i = 1; i <= max_idx-1; i++)
+  {
+    if (scan->ranges[i] < closest_range_left)
+    {
+      closest_range_left = scan->ranges[i];
+    }
+  }
+  // scan the left side
+  for (int i = scan_count-min_idx; i <= scan_count-1 ; i++)
+  {
+    if (scan->ranges[i] < closest_range_right)
+    {
+      closest_range_right = scan->ranges[i];
+    }
+  }
+
+  if (closest_range_right < MIN_PROXIMITY_RANGE_M)
+  {
+    angleFactor = +1;
+    doMove = false;
+  } else if (closest_range_left  < MIN_PROXIMITY_RANGE_M)
+  {
+    angleFactor = 1;
+    doMove = false;
+  } else {
+    doMove = true;
+  }
 }
--- src/driver/src/StateRandomWalk.h
+++ src/driver/src/StateRandomWalk.h
@@ -1,14 +1,24 @@
 #pragma once
 
 #include "State.h"
+#include "sensor_msgs/LaserScan.h"
 
 class StateRandomWalk : public State
 {
 public:
-  StateRandomWalk();
+  StateRandomWalk(std::shared_ptr<TurtleBot> turtleBot);
 
   virtual void tick();
 
-private:
+  constexpr static double MIN_SCAN_ANGLE_RAD = -25.0/180*M_PI;
+	constexpr static double MAX_SCAN_ANGLE_RAD = +25.0/180*M_PI;
+  constexpr static float TURN_SPEED = 0.2f;
+  constexpr static float MOVEMENT_SPEED = 0.2f;
+  constexpr static float MIN_PROXIMITY_RANGE_M = 0.7;
 
+private:
+  void scanCallback(const	sensor_msgs::LaserScan::ConstPtr& scan);
+
+  bool doMove = true;
+  int angleFactor = 1;
 };
